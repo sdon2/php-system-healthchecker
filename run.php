4e16044110ed76e4df0ebe28ccac0b4a1ad8f61f
Saravanakumar Arumugam <saravanakumar.a.o@gmail.com>
2022-02-24 23:16:24 +0530

    Update run.php

--- run.php
+++ run.php
@@ -10,41 +10,49 @@ use HealthChecker\Contracts\IHealthCheck;
 use HealthChecker\HealthChecks\SwapHealthCheck;
 use HealthChecker\Mailer;
 
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__.'/.env');
+try
+{
+    $dotenv = new Dotenv();
+    $dotenv->load(__DIR__.'/.env');
 
-$checks = [
-    PhysicalMemoryHealthCheck::class,
-    SwapHealthCheck::class,
-];
+    $checks = [
+        PhysicalMemoryHealthCheck::class,
+        SwapHealthCheck::class,
+    ];
 
-$test_passed = true;
-$report = "";
+    $test_passed = true;
+    $report = "";
 
-foreach ($checks as $check) {
-    /**
-     * @var IHealthCheck $class
-     */
-    $class = new $check();
+    foreach ($checks as $check) {
+        /**
+        * @var IHealthCheck $class
+        */
+        $class = new $check();
 
-    if ($class->isThresholdFailed()) {
-        $report .= $class->getHealthCheckName() . " exceeded ThresHold\n";
-        $report .= $class->getReport();
+        if ($class->isThresholdFailed()) {
+            $report .= $class->getHealthCheckName() . " exceeded ThresHold\n";
+            $report .= $class->getReport();
 
-        $test_passed = false;
+            $test_passed = false;
+        }
+    }
+
+    if ($test_passed) {
+        echo "\nAll tests passed!!!\n";
+        exit(0);
+    } else {
+        if ($_ENV['ENABLE_EMAIL'] != 'false') {
+            echo "\nSome of the Tests failed and Email Triggered!!!\n";
+            Mailer::sendReport($report);
+        } else {
+            echo "\nSome of the Tests Failed!!!\n";
+            echo $report;
+        }
+        exit(-1);
     }
 }
-
-if ($test_passed) {
-    echo "\nAll tests passed!!!\n";
-    exit(0);
-} else {
-    if ($_ENV['ENABLE_EMAIL'] != 'false') {
-        echo "\nSome of the Tests failed and Email Triggered!!!\n";
-        Mailer::sendReport($report);
-    } else {
-        echo "\nSome of the Tests Failed!!!\n";
-        echo $report;
-    }
+catch (\Exception $ex) {
+    echo "\n" . $ex->getMessage() . "\n";
     exit(-1);
 }
+
